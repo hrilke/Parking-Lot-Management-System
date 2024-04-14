7c7a39607316b36ae41d1f33f2f5f8edde9280de
Hrishikesh Shelke <dev.hrishi@outlook.com>
2024-04-15 03:29:26 +0530

    error fixed

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("WELCOME TO Parking Lot Management System")git
+        System.out.println("WELCOME TO Parking Lot Management System");
     }
 }
